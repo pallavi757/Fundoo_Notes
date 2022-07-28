4d6683038cb9567a6938aa9d9d3060996b8fe42a
unknown <kinekarpallavi21@gmail.com>
2022-07-28 22:51:21 +0530

    user registration done

--- fundooNotes/src/models/user.model.js
+++ fundooNotes/src/models/user.model.js
@@ -2,9 +2,10 @@ import { Schema, model } from 'mongoose';
 
 const userSchema = new Schema(
   {
-    name: {
-      type: String
-    }
+    firstName: {type: String,required:true},
+    lastName: {type: String,required:true},
+    email: {type: String,required:true,unique:true},
+    password:{type: String,required:true},
   },
   {
     timestamps: true
--- fundooNotes/src/validators/user.validator.js
+++ fundooNotes/src/validators/user.validator.js
@@ -2,11 +2,18 @@ import Joi from '@hapi/joi';
 
 export const newUserValidator = (req, res, next) => {
   const schema = Joi.object({
-    name: Joi.string().min(4).required()
+    firstName: Joi.string().min(2).required(),
+    lastName: Joi.string().min(2).required(),
+    email: Joi.string().email().trim(true).required(),
+    password: Joi.string().length(8).required() 
   });
   const { error, value } = schema.validate(req.body);
   if (error) {
-    next(error);
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      message: `${error}`
+    });
+    //next(error);
   } else {
     req.validatedBody = value;
     next();
