723f9f51c910571096bf896c7aa981b5b4f123ee
unknown <kinekarpallavi21@gmail.com>
2022-08-01 23:35:24 +0530

    CURD opertion done on note

--- fundooNotes/src/validators/note.validator.js
+++ fundooNotes/src/validators/note.validator.js
@@ -1,18 +1,15 @@
 import Joi from '@hapi/joi';
-import HttpStatus from 'http-status-codes';
 export const noteValidator = (req, res, next) => {
     const schema = Joi.object({
         Title: Joi.string().min(4).alphanum().required(),
         Descreption: Joi.string().min(4).required(),
-        color: Joi.string().optional()
+        color: Joi.string().optional(),
+        isArchived:Joi.boolean().optional(),
+        isDeleted:Joi.boolean().optional()
     });
     const { error, value } = schema.validate(req.body);
   if (error) {
     next(error);
-    // res.status(HttpStatus.BAD_REQUEST).json({
-    //   code: HttpStatus.BAD_REQUEST,
-    //   message: `${error}`
-    //});
   } else {
     next();
   }
